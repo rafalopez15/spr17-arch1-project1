beef3ec1db21d93b88960c9568ad96592e5576e1
Rafa <relopez6@miners.utep.edu>
2017-03-03 14:10:19 -0700

    Fixed errors

--- TreeNode.c
+++ TreeNode.c
@@ -3,42 +3,58 @@
 #include "TreeNode.h"
 
 /* Method to create root of tree */
-TreeNode *Tree() {
-  TreeNode *root;
-  root = malloc(sizeof(TreeNode));
-  root->name = 0;
-  root->left = NULL;
-  root->right = NULL;
-  return root;
-}
-
-/* Create node function */
-TreeNode *createNode(char *name) {
-  TreeNode *node;
-  node = malloc(sizeof(TreeNode));
-  node->name = name;
-  node->left = NULL;
-  node->right = NULL;
-  return node;
+TreeNode *Tree(void) {
+  return (TreeNode *)malloc(sizeof(TreeNode));
 }
 
 /* Adding an employee to the tree */
-void insert(TreeNode *tp, char *name) {
+TreeNode *insertEmployee(TreeNode *tp, char *s) {
   if (tp == NULL) {
-    createNode(name);
+    tp = Tree();
+    tp->name = stringCopy(s);
+    tp->left = tp->right = NULL;
   } else {
-    if (compare(tp->name, name) < 0)
-      insert(tp->left, name);
-    else if (compare(tp-name, name) > 0)
-      insert(tp->right, name);
-    else if (compare(tp-name, name) == 0)
-      printf("User already exists.");
+    if (compare(tp->name, s) > 0) {
+      //  printf("GOING LEFT\n");
+      tp->left = insertEmployee(tp->left, s);
+    } else if (compare(tp->name, s) < 0) {
+      // printf("GOING RIGHT\n");
+      tp->right = insertEmployee(tp->right, s);
+    } else
+      printf("User already exists.\n");
+  }
+  return tp;
+}
+
+/* Printing the Tree in order */
+void printTree(TreeNode *tp) {
+  if (tp != NULL) {  
+    printTree(tp->left);
+    printf("%s\n", tp->name);
+    printTree(tp->right);
   }
 }
 
 /* Fucntion to compare two strings */
 int compare(char *name1, char *name2) {
   for ( ; *name1 == *name2; name1++, name2++)
-    if (*s =='\0') return 0;
+    if (*name1 =='\0') return 0;
   return *name1 - *name2;
+}
+
+/* Function to make a copy of a string */
+char *stringCopy(char *s) {
+  char *nameCopy;
+  int len;
+  
+  for (len = 0; s[len]; len++)
+    ;
+  
+  nameCopy = (char *)malloc(sizeof(len + 1));
+  
+  for (len = 0; s[len]; len++)
+    nameCopy[len] = s[len];
+  
+  nameCopy[len] = 0;
+  return nameCopy;
 }
